2cbdfbf6f7ddbf877ad2a76cb640720a6b1a69bc
Edgar Morales <edmorales94@gmail.com>
2019-10-19 16:41:08 -0400

    Loads the function that fades in and out the wine glasses

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function preloader() {
+    let loader = document.getElementById("status");
+    let loaderContainer = document.getElementById("preloader");
+    loader.classList.forEach(function (el) {
+        console.log(el)
+    });
+    if (loader.className.indexOf('fade-in-out')!==1){
+       loader.classList.add('fade-in-out');
+    }
+    setTimeout(function () {
+        loaderContainer.remove()
+    }, 2500);
+}
